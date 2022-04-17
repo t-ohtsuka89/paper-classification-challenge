2c0df0eeecf0da6c45e065412d606ac4a8dfac66
t-ohtsuka89 <102138393+t-ohtsuka89@users.noreply.github.com>
2022-04-18 00:24:17 +0900

    add comment for linter.

--- utils/seed.py
+++ utils/seed.py
@@ -12,5 +12,5 @@ def seed_everything(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True  # type: ignore
+    torch.backends.cudnn.benchmark = False  # type: ignore
